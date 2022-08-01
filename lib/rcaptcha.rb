e71aa111bc0feadd8b33825ed141e99a3b9eca71
Ian <cameronian0@protonmail.com>
2022-08-01 18:02:03 +0800

    Remove deprecated warning on RMagick import

--- lib/rcaptcha.rb
+++ lib/rcaptcha.rb
@@ -1,10 +1,14 @@
-require 'RMagick'
+require 'rmagick'
 require 'rcaptcha/generator'
 
 module RCaptcha
 
   def self.generate(*args)
     RCaptcha::Generator.generate *args
+  end
+
+  def self.generate_image(*args)
+    RCaptcha::Generator.generate_image *args
   end
 
 end
